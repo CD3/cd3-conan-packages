c8ad11fa27b7f761f8a79d7bb2a811b78dd4352b
C.D. Clark III <clifton.clark@gmail.com>
2019-05-22 20:39:38 -0500

    feat(test-integrations.py): added --test option to specify packages to test at command line.

--- bin/test-integrations.py
+++ bin/test-integrations.py
@@ -20,15 +20,15 @@ parser.add_argument(
     nargs="*",
     help="File(s) contianing configurations to test.",
 )
-parser.add_argument(
-    "--parallel", dest="parallel", action="store_true", help="Run jobs in parallel"
-)
-parser.add_argument(
-    "--no-parallel",
-    dest="parallel",
-    action="store_false",
-    help="Do not run jobs in parallel",
-)
+# parser.add_argument(
+    # "--parallel", dest="parallel", action="store_true", help="Run jobs in parallel"
+# )
+# parser.add_argument(
+    # "--no-parallel",
+    # dest="parallel",
+    # action="store_false",
+    # help="Do not run jobs in parallel",
+# )
 parser.add_argument(
     "--unit-tests", dest="do_unit_tests", action="store_true", help="Attempt to run the unit tests for all packages being tested after they are built."
 )
@@ -49,6 +49,11 @@ parser.add_argument(
     help="Print the configuration that will be used..",
 )
 parser.add_argument(
+    "--test", "-t",
+    action="append",
+    help="Packages to tests. This will override any packages specified in the configuration file.",
+)
+parser.add_argument(
     "--profile",
     action="store",
     default="default",
@@ -61,15 +66,14 @@ args = parser.parse_args()
 prog_path = Path(parser.prog)
 
 
-def test_package(global_config, package_name, do_unit_tests=True):
+def test_package(scratch_folder_path, package_name, do_unit_tests=True):
     pdir = Path(package_name)
     conanfile = pdir / "conanfile-static.py"
     if not conanfile.is_file():
         conanfile = pdir / "conanfile.py"
-    test_dir = global_config[prog_path.stem]["scratch-folder"]
-    source_dir = test_dir
-    build_dir = Path(source_dir) / pdir / "build"
-    outfile = Path(test_dir) / f"{package_name}.out"
+    source_dir = scratch_folder_path
+    build_dir = source_dir / pdir / "build"
+    outfile = scratch_folder_path / f"{package_name}.out"
 
     with open(outfile, "w") as f:
 
@@ -100,8 +104,6 @@ def test_package(global_config, package_name, do_unit_tests=True):
                 + util.EOL
             )
             f.write("\n\n")
-            f.write("Configuration:\n")
-            yaml.dump(global_config, f)
             return 1
 
         print(package_name + ":Building with 'conan build ...'")
@@ -155,7 +157,7 @@ if __name__ == "__main__":
     pc.baseline_config[prog_path.stem] = dict()
     pc.baseline_config[prog_path.stem]["packages_to_test"] = "all"
     pc.baseline_config[prog_path.stem]["packages_to_export"] = "all"
-    pc.baseline_config[prog_path.stem]["scratch-folder"] = "_test-integrations.d"
+    pc.baseline_config[prog_path.stem]["scratch-folder"] = "_test-integrations-{}.d"
     if args.print_default_configuration:
         print("# Default Configuration")
         print(yaml.dump(pc.baseline_config))
@@ -163,10 +165,10 @@ if __name__ == "__main__":
 
     pc.load_configs(args.configuration)
 
-    test_folder_path = Path(pc.config[prog_path.stem]["scratch-folder"])
-    if test_folder_path.exists():
-        shutil.rmtree(str(test_folder_path))
-    Path(pc.config[prog_path.stem]["scratch-folder"]).mkdir()
+    scratch_folder_path = Path(pc.config[prog_path.stem]["scratch-folder"].format(args.profile))
+    if scratch_folder_path.exists():
+        shutil.rmtree(str(scratch_folder_path))
+    scratch_folder_path.mkdir()
 
     pc.add_packages(package_paths)
     if args.print_configuration:
@@ -177,18 +179,21 @@ if __name__ == "__main__":
     packages_to_export = pc.filter_packages(
         pc.config[prog_path.stem].get("packages_to_export", "all")
     )
-    with (Path(pc.config[prog_path.stem]["scratch-folder"]) / "conan_export.out").open(
+    with (scratch_folder_path / "conan_export.out").open(
         "w"
     ) as f:
         print("Exporting packages: " + ", ".join(packages_to_export))
         results = [pc.export_package(package, f) for package in packages_to_export]
         print("Done")
 
-    packages_to_test = pc.filter_packages(
-        pc.config[prog_path.stem].get("packages_to_test", "all")
-    )
+    if args.test:
+      packages_to_test = args.test
+    else:
+      packages_to_test = pc.filter_packages(
+          pc.config[prog_path.stem].get("packages_to_test", "all")
+      )
     print("Testing packages: " + ", ".join(packages_to_test))
-    results = [test_package(pc.config, package, args.do_unit_tests) for package in packages_to_test]
+    results = [test_package(scratch_folder_path, package, args.do_unit_tests) for package in packages_to_test]
     print("Done")
 
     num_tests = len(results)
