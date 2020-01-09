6825161bf0957c42e98ff35ef92ea56fc981516e
C.D. Clark III <clifton.clark@gmail.com>
2020-01-09 09:28:44 -0600

    bumped UnitConvert version

--- recipes/UnitConvert/conanfile.py
+++ recipes/UnitConvert/conanfile.py
@@ -4,8 +4,8 @@ import os, glob
 class ConanPackage(ConanFile):
     name = "UnitConvert"
     git_url_basename = "git://github.com/CD3"
-    version = "0.6"
-    checkout = "0.6"
+    version = "0.7"
+    checkout = "0.7"
 
     author = "CD Clark III clifton.clark@gmail.com"
     description = "A C++ library for runtime unit conversions."
