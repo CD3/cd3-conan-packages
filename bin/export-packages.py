6bfea3f234834fbb0f62509b3137d36ea059d545
C.D. Clark III <clifton.clark@gmail.com>
2020-06-10 14:45:40 -0500

    updated export and testing scripts.

--- bin/export-packages.py
+++ bin/export-packages.py
@@ -2,6 +2,7 @@
 import shutil
 import sys
 import yaml
+import pprint
 from pathlib import Path
 
 import util
@@ -13,18 +14,101 @@ prog_path = Path(sys.argv[0]).resolve()
 @click.command()
 @click.option("--print-default-configuration", help ="Print the default configuration that will be used and exit.",is_flag=True)
 @click.option("--print-configuration",         help ="Print the actual configuration that will be used and exit.", is_flag=True)
-@click.option("-p","--package",                help ="Select subset of packages to export.",multiple=True)
+@click.option("-p","--packages",               help ="Comma-separated list of packages to export.")
 @click.option("-o","--owner",                  help ="Set owner to to use for package export.")
 @click.option("-c","--channel",                help ="Set channel to to use for package export.")
 @click.option("--create",                      help ="Create packages (build and pacakage) instead of just exporting.",is_flag=True)
 @click.argument("config_file", required=False, nargs=-1, type=click.Path())
-def main(print_default_configuration,print_configuration,package,owner,channel,create,config_file):
+def main(print_default_configuration,print_configuration,packages,owner,channel,create,config_file):
   """
   Export conan packages to the local cache.
 
   This script exports conan recipes to the local cache by default. Consumers
   can then use the packages, but must specify `--build missing` the first time
   they install.
+
+  This script allows conan package recipes to be generated from a base recipe. For example, if you wanted to create
+  a package for the last three major releases of LibA, the recipe files for each version would probably look identical
+  expect for the version number and the commit that gets checked out (unless the build system changes). Rather than
+  write three different recipes, you can just write a single recipe for the latest release. This script can then
+  generate recipes for the other versions.
+
+  To do this, every package that gets exported by this script starts with a
+  base recipe. You can then specify settings to override (see Configuration
+  Settings below) to create a new recipe instance. If no override settings are
+  specified, then the recipe instance will just be the base recipe.
+
+        base recipe -> [override settigns] -> recipe instance
+
+  This is convenient because it allows you to maintain a collection of valid
+  recipe files that can be tested and used with just conan, and then use this
+  script to export variations on these recipes.
+  
+
+  Configuration:
+
+  The exported packages can be configured using a YAML file and passing it as an argument to this script.
+
+  Configuration Settings:
+
+  \b
+  global
+      a namespace for global package settings (setting that will be applied to all packages)
+  global.export
+      a namespace for setting related to exporting packages.
+  global.export.owner
+      the default package owner
+  global.export.channel
+      the default package channel
+  package_instances
+      a namespace used for overriding package settings on a per-package basis.
+  package_instances[i].conanfile
+      path to the conan recipe to use.
+  package_instances[i].name
+      name used to refer to package in the script.
+      this is not the name that will be used in the package recipe (see settings_overrides)
+  package_instances[i].setting_overrides
+      a dict of key/value pairs that will be added to the recipe instance to override settings in the base recipe.
+      the key/value pairs are arbitrary. if the key is a setting in the base recipe, it will be overridden. if it is
+      not, it will be added.
+  package_instances[i].setting_overrides.name
+      override the package name
+  package_instances[i].setting_overrides.version
+      override the package version
+  package_instances[i].setting_overrides.checkout
+      override the git reference that will be checked out to build the package. must be supported by the base recipe.
+  export-packages
+      a namespace used for setting for this script
+  export-packages.scratch-folder
+      the name of the directory used for writing data to disk (logs, temp files, etc)
+  export-packages.packages_to_export
+      a list of packages to export. by default, all packages will be exported.
+      this will be overridden by the --packages option.
+
+  Example Configuration File:
+
+  \b
+  export-packages:
+    packages_to_export: all
+    scratch-folder: _package-exports.d
+  global:
+    export:
+      channel: devel
+      owner: MyOrg
+  package_instances:
+  - conanfile: /path/to/LibA/conanfile.py
+    name: LibA
+    setting_overrides:
+      version: "3.1"
+      checkout: "v3.1"
+  - conanfile: /path/to/LibA/conanfile.py
+    name: LibA
+    setting_overrides:
+      version: "3.2"
+      checkout: "v3.2"
+
+  This will create and export a recipe instance for every conanfile.py found in the recipies directory,
+  and two recipe instances for the LibA package.
   """
 
   default_configuration_file = prog_path.parent / f"{prog_path.stem}-default-config.yaml"
@@ -35,14 +119,14 @@ def main(print_default_configuration,print_configuration,package,owner,channel,c
     print(util.WARN + f"WARNING: did not find default configuration file '{str(default_configuration_file)}'." + util.EOL)
 
 
-  config = yaml.load( default_configuration_text, Loader=yaml.SafeLoader )
+  config = yaml.load( default_configuration_text, Loader=yaml.BaseLoader )
   if print_default_configuration:
     print("# Default Configuration")
     print(yaml.dump(config))
     sys.exit(0)
 
   for file in config_file:
-    util.update_dict( config, yaml.load( Path(file).read_text(), Loader=yaml.SafeLoader ) )
+    util.update_dict( config, yaml.load( Path(file).read_text(), Loader=yaml.BaseLoader ) )
 
   
   if not 'global' in config:
@@ -56,11 +140,32 @@ def main(print_default_configuration,print_configuration,package,owner,channel,c
   if channel:
     config['global']['export']['channel'] = channel
 
+  if packages:
+    if packages in ['all','instances-only']:
+      config[prog_path.stem]['packages_to_export'] = packages
+    else:
+      config[prog_path.stem]['packages_to_export'] = packages.split(",")
+
+  packages_to_export = config[prog_path.stem].get('packages_to_export','all')
+
+
 
   if not 'package_instances' in config:
     config['package_instances'] = list()
+
+  # sort of a hack. we want to let the user specify that only the instances explicitly listed
+  # should be exported
+  if packages_to_export != "instances-only":
     for file in Path("recipes").glob("*/conanfile.py"):
       config['package_instances'].append( {'conanfile':str(file.absolute()), 'name' : str(file.parent.stem) } )
+    for file in Path("recipes").glob("*/conanfile-latest.py"):
+      config['package_instances'].append( {'conanfile':str(file.absolute()), 'name' : str(file.parent.stem) } )
+  else:
+    packages_to_export = "all"
+
+  # make sure all conanfiles are specified with absolute path
+  for instance in config["package_instances"]:
+    instance["conanfile"] = str(Path(instance["conanfile"]).absolute())
 
   if print_configuration:
     print("# Complete Configuration")
@@ -75,10 +180,7 @@ def main(print_default_configuration,print_configuration,package,owner,channel,c
     shutil.rmtree(str(scratch_folder_path))
   scratch_folder_path.mkdir()
 
-  if package:
-    packages_to_export = [ p.name for p in util.filter_packages( {'include' : list(package) }, pc.package_instances ) ]
-  else:
-    packages_to_export = [ p.name for p in util.filter_packages( pc.config[prog_path.stem].get('packages_to_test','all'), pc.package_instances ) ]
+  packages_to_export = [ p.name for p in util.filter_packages( packages_to_export, pc.package_instances ) ]
 
   if create:
     print("Creating packages")
