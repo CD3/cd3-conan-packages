7b432d78b80241c03f8715b6830319bffa840485
CD Clark III <clifton.clark@gmail.com>
2021-05-16 11:24:42 -0500

    bumped libIntegrate version

--- recipes/libIntegrate/conanfile.py
+++ recipes/libIntegrate/conanfile.py
@@ -9,8 +9,8 @@ import re
 class ConanPackage(ConanFile):
     name = "libIntegrate"
     git_url_basename = "git://github.com/CD3"
-    version = "0.5.1"
-    checkout = "v0.5.1"
+    version = "1.0"
+    checkout = "v1.0"
 
     author = "CD Clark III clifton.clark@gmail.com"
     description = "A C++ library for numerical integration supporting multiple methods/algorithms."
