f32c781e5019b9a0a89eecbcd0877e8a7ef48dd0
CD Clark III <clifton.clark@gmail.com>
2022-02-10 17:21:22 -0600

    added latest version of libInterpolate.

--- recipes/libInterpolate/conanfile.py
+++ recipes/libInterpolate/conanfile.py
@@ -9,8 +9,8 @@ import re
 class ConanPackage(ConanFile):
     name = "libInterpolate"
     git_url_basename = "git://github.com/CD3"
-    version = "2.5"
-    checkout = "2.5"
+    version = "2.6"
+    checkout = "2.6"
 
     author = "CD Clark III clifton.clark@gmail.com"
     description = "A C++ library for numerical interpolation supporting multiple methods/algorithms."
