ae444833d888be57f3ea3dede08a3b87d0bd9fd7
CD Clark III <clifton.clark@gmail.com>
2024-11-06 09:01:53 -0600

    refactor: removed recipe for boost unit definitions. this is in conan-center now.

--- recipes/BoostUnitDefinitions/all/test_package/example.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include <BoostUnitDefinitions/Units.hpp>
-
-int main()
-{
-  boost::units::quantity<boost::units::t::meter> L(2*boost::units::i::meter);
-
-  std::cout << L << std::endl;
-}
